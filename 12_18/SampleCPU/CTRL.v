f313dc2d2915fccd27f7614e4414b4c0e396e090
1077646197 <167066966+1077646197@users.noreply.github.com>
2025-12-29 15:04:33 +0800

    更新NeuCpu项目：包含12_18和12_26两个版本的SampleCPU实现，完整的MIPS五级流水线CPU设计

--- 12_18/SampleCPU/CTRL.v
+++ 12_18/SampleCPU/CTRL.v
@@ -1,8 +1,8 @@
 `include "lib/defines.vh"
 module CTRL(
     input wire rst,
-    // input wire stallreq_for_ex,
-    // input wire stallreq_for_load,
+    input wire stallreq_for_ex,
+    input wire stallreq_for_load,
 
     // output reg flush,
     // output reg [31:0] new_pc,
@@ -12,9 +12,17 @@ module CTRL(
         if (rst) begin
             stall = `StallBus'b0;
         end
+        else if (stallreq_for_load==`Stop) begin
+            stall = `StallBus'b000111;
+        end
+        else if (stallreq_for_ex==`Stop) begin
+            stall = `StallBus'b001111;
+        end
         else begin
             stall = `StallBus'b0;
         end
     end
+    
+ 
 
 endmodule
